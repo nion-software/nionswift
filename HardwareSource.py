48b0bfffa8b879e1b9e2990d09fde6e6120a137e
Chris Meyer <cmeyer1969@gmail.com>
2013-10-16 00:06:29 +0000

    Change hardware source to use data elements (dicts) instead of data directly.
    svn r844

--- HardwareSource.py
+++ HardwareSource.py
@@ -1,20 +1,20 @@
 """
-This module defines a couple of classes proposed as a framework for handling live image
+This module defines a couple of classes proposed as a framework for handling live data
 sources.
 
-A HardwareSource represents a source of images.
-A client accesses images through the create_port function
+A HardwareSource represents a source of data elements.
+A client accesses data_elements through the create_port function
 which changes the mode of the source, and starts it, and starts notifying the returned
-port object. The port object should not do any significant processing in its on_new_image
+port object. The port object should not do any significant processing in its on_new_data_elements
 function but should instead just notify another thread that new data is available.
 
 The hardware source does allow processing to happen at acquisition time, by setting its
-filter member to a method that takes an image and returns an image.
+filter member to a method that takes a data element and returns a data element.
 
 One idea is that there would be a Screen class, containing multiple ports, which would
 contain a single pre-display processing thread. This thread would listen to messages posted
-by the ports getting a new image, then get the data from the port, process to make
-compatible with the Screen, then push the images into it.
+by the ports getting a new data element, then get the data from the port, process to make
+compatible with the Screen, then push the data_elements into it.
 
 There's also a LoggableCodeBlock class showing one way of combining functionality with
 a full description of the code being called. It allows expressions to be added one
@@ -43,7 +43,7 @@ class HardwareSourceManager(object):
     """
     Keeps track of all registered hardware sources.
     Also keeps track of aliases between hardware sources and logical names.
-    The aliases can contain a filter to only return a subset of images.
+    The aliases can contain a filter to only return a subset of data_elements.
     This way we can create an alias for the 'RonchigramCamera' to, say,
     NionCCD1010, channel 0, properties="Tuning", and HAADF Signal to
     superscan, channel 1 (we can override the properties when we create the
@@ -55,36 +55,36 @@ class HardwareSourceManager(object):
 
     """
     def __init__(self):
-        self._all_hw_ports = []
+        self._hardware_ports = []
         self._aliases = {}
         # we create a list of callbacks for when a hardware
         # source is added or removed
         self.hardware_source_added_removed = []
 
     def _reset(self):  # used for testing to start from scratch
-        self._all_hw_ports = []
+        self._hardware_ports = []
         self._aliases = {}
         self.hardware_source_added_removed = []
 
     def register_hardware_source(self, hw_src):
-        self._all_hw_ports.append(hw_src)
+        self._hardware_ports.append(hw_src)
         for f in self.hardware_source_added_removed:
             f(self, None)
 
     def unregister_hardware_source(self, hw_src):
-        self._all_hw_ports.remove(hw_src)
+        self._hardware_ports.remove(hw_src)
         for f in self.hardware_source_added_removed:
             f(self, None)
 
-    def create_port_for_name(self, owner, hw_source_name, override_props=None):
+    def create_port_for_name(self, owner, hardware_source_name, override_props=None):
         """
         Alias for HardwareSource.create_port, with the added benefit
         of looking up aliases.
 
-        Returns a newly created port for the hw_source_name given.
-        If hw_source_name is the name of a hardware source,
+        Returns a newly created port for the given hardware_source_name.
+        If hardware_source_name is the name of a hardware source,
         it will return that source, with the port listening for all channels.
-        If hw_source_name is an alias, it will return the hardware source it
+        If hardware_source_name is an alias, it will return the hardware source it
         refers to with the filter and props registered for that alias.
 
         Owner is an object whose string representation is used to indicate
@@ -98,18 +98,19 @@ class HardwareSourceManager(object):
         ...
         port.close()
         """
-        if hw_source_name in self._aliases:
-            hw_source_name, props, filter = self._aliases[hw_source_name]
+        if hardware_source_name in self._aliases:
+            hardware_source_name, props, filter = self._aliases[hardware_source_name]
         else:
             props, filter = "Default", None
 
         if override_props:
             props = override_props
 
-        hw_source_name = str(hw_source_name)
-        for w in self._all_hw_ports:
-            if str(w) == hw_source_name:
-                return w.create_port(owner, props, filter)
+        hardware_source_name = str(hardware_source_name)
+        for hardware_port in self._hardware_ports:
+            if str(hardware_port) == hardware_source_name:
+                return hardware_port.create_port(owner, props, filter)
+
         return None
 
     def make_hardware_source_alias(self, alias, hardware_source, props="Default", filter=None):
@@ -120,7 +121,7 @@ class HardwareSourceManager(object):
         Returns a string representation of all registered sources.
         If include_aliases is true, also includes the registered aliases.
         """
-        ret = [str(s) for s in self._all_hw_ports]
+        ret = [str(s) for s in self._hardware_ports]
         if include_aliases:
             ret += self._aliases.keys()
         return ret
@@ -128,13 +129,13 @@ class HardwareSourceManager(object):
 
 class HardwareSource(object):
     """
-    A hardware source provides ports, which in turn provide images.
+    A hardware source provides ports, which in turn provide data_elements.
 
     Ports are created based on asked-for properties. The current properties of
     the HW source are always those most recently requested, ie the properties
     of the last port created. A filter can optionally be passed to a port which
-    selects which of the returned list of images to return. It should be either
-    a single index or a list of indices. If None, all images are returned.
+    selects which of the returned list of data_elements to return. It should be either
+    a single index or a list of indices. If None, all data_elements are returned.
 
     A separate acquisition thread is used for acquiring all data and
     passing on to the ports. When a HardwareSource has no ports, this
@@ -142,33 +143,33 @@ class HardwareSource(object):
     stop_acquisition and set_from_props functions are always only ever
     called from the acquisition thread
     """
-    class HWSourcePort():
+    class HardwareSourcePort():
         def __init__(self, name, props, parent, filter):
             self.name = name
             self.props = props
             self.parent_source = parent
-            self.on_new_images = None
-            self.last_images = None
+            self.on_new_data_elements = None
+            self.last_data_elements = None
             self.filter = filter
             if isinstance(self.filter, numbers.Integral):
                 self.filter = (self.filter, )
 
-        def want_image(self, props):
+        def want_data_element(self, props):
             """
-            Return True if we will accept an image with the given properties.
+            Return True if we will accept a data element with the given properties.
             """
             return True
 
-        def get_last_images(self):
-            return self.last_images
+        def get_last_data_elements(self):
+            return self.last_data_elements
 
-        def _set_new_images(self, images):
+        def _set_new_data_elements(self, data_elements):
             if self.filter:
-                self.last_images = [ims for i, ims in enumerate(images) if i in self.filter]
+                self.last_data_elements = [data_element for i, data_element in enumerate(data_elements) if i in self.filter]
             else:
-                self.last_images = images
-            if self.on_new_images:
-                self.on_new_images(self.last_images)
+                self.last_data_elements = data_elements
+            if self.on_new_data_elements:
+                self.on_new_data_elements(self.last_data_elements)
 
         def close(self):
             """
@@ -183,7 +184,7 @@ class HardwareSource(object):
 
     def create_port(self, name, props, filter=None):
         with self.__portlock:
-            ret = HardwareSource.HWSourcePort(name, props, self, filter)
+            ret = HardwareSource.HardwareSourcePort(name, props, self, filter)
             start_thread = len(self.ports) == 0  # start a new thread if it's not already running (i.e. there are no current ports)
             self.ports.append(ret)
             self.set_from_props(props)
@@ -205,7 +206,7 @@ class HardwareSource(object):
         self.start_acquisition()
         try:
             last_props = None
-            new_images = None
+            new_data_elements = None
 
             while True:
                 with self.__portlock:
@@ -217,17 +218,17 @@ class HardwareSource(object):
                         self.set_from_props(last_props)
 
                     for port in self.ports:
-                        # check to make sure we actually have new images,
+                        # check to make sure we actually have new data elements,
                         # since this might be the first time through the loop.
-                        # otherwise the image list gets cleared and new images
-                        # get created when the images become available.
-                        if port.want_image(last_props) and new_images:
-                            port._set_new_images(new_images)
+                        # otherwise the data element list gets cleared and new data elements
+                        # get created when the data elements become available.
+                        if port.want_data_element(last_props) and new_data_elements:
+                            port._set_new_data_elements(new_data_elements)
 
-                new_images = self.acquire()
+                new_data_elements = self.acquire_data_elements()
 
-                # new_images should never be empty
-                assert new_images
+                # new_data_elements should never be empty
+                assert new_data_elements
         finally:
             self.stop_acquisition()
 
@@ -240,9 +241,9 @@ class HardwareSource(object):
     # subclasses are expected to implement this function efficiently since it will
     # be repeatedly called. in practice that means that subclasses MUST sleep (directly
     # or indirectly) unless the data is immediately available, which it shouldn't be on
-    # a regular basis. it is an error for this function to return an empty list of images.
-    def acquire(self):
-        raise NotImplementedError("HardwareSource.acquire must be implemented")
+    # a regular basis. it is an error for this function to return an empty list of data_elements.
+    def acquire_data_elements(self):
+        raise NotImplementedError()
 
     def set_from_props(self, props):
         pass
@@ -255,13 +256,13 @@ class HardwareSourceDataBuffer(object):
     """
     For the given HWSource (which can either be an object with a create_port function, or a name. If
     a name, ports are created using the HardwareSourceManager.create_port_for_name function,
-    and aliases can be supplied), creates a port and listens for any images.
-    Manages a collection of DataItems for all images returned.
+    and aliases can be supplied), creates a port and listens for any data_elements.
+    Manages a collection of DataItems for all data_elements returned.
     The DataItems are owned by this, and persist while the acquisition is live,
     if the user wants to remove the data items, they should stop the acquisition
     first.
 
-    DataItems are resued if available - we reuse them by searching through all images
+    DataItems are resued if available - we reuse them by searching through all data_elements
     in the 'Sources' data_group, based on name (could use more advanced metadata in the
     future f necessary).
 
@@ -287,10 +288,10 @@ class HardwareSourceDataBuffer(object):
             # we should be consistent about how we stop live acquisitions:
             # stopping should be identical to acquiring with no channels selected
             # and we should delete/keep data items the same in both cases.
-            # esaiest way is to call on_new_images(None)
+            # esaiest way is to call on_new_data_elements(None)
             # now that we've set hardware_port to None
-            # if we do want to keep data items, it should be done in on_new_images
-            self.on_new_images([])
+            # if we do want to keep data items, it should be done in on_new_data_elements
+            self.on_new_data_elements([])
 
     # Add a listener. Listeners will receive data_item_changed message when this
     # DataItem is notified of a change via the notify_data_item_changed() method.
@@ -325,7 +326,7 @@ class HardwareSourceDataBuffer(object):
             current_snapshot = len(self.__snapshots) - 1
         if self.__current_snapshot != current_snapshot:
             self.__current_snapshot = current_snapshot
-            self.update_images(self.__snapshots[self.__current_snapshot])
+            self.update_data_elements(self.__snapshots[self.__current_snapshot])
             self.notify_listeners("current_snapshot_changed", self.hardware_source, self.__current_snapshot)
     current_snapshot = property(__get_current_snapshot, __set_current_snapshot)
 
@@ -341,39 +342,33 @@ class HardwareSourceDataBuffer(object):
             else:
                 self.hardware_port = self.hardware_source_man.create_port_for_name(
                     "LiveHWPortToImageSource", str(self.hardware_source))
-            self.hardware_port.on_new_images = self.on_new_images
+            self.hardware_port.on_new_data_elements = self.on_new_data_elements
             self.first_data = True
 
     def pause(self):
         logging.info("Pausing HardwareSourceDataBuffer for %s", str(self.hardware_source))
         if self.hardware_port is not None:
-            self.hardware_port.on_new_images = None
+            self.hardware_port.on_new_data_elements = None
             self.hardware_port.close()
-            # finally we remove the reference to the port. on_new_images needs it around
+            # finally we remove the reference to the port. on_new_data_elements needs it around
             # above to get the name of any existing data_items
             self.hardware_port = None
         for channel in self.last_channel_to_data_item_dict.keys():
             data_item = self.last_channel_to_data_item_dict[channel]
             data_item.live_data = False
 
-    def on_new_images(self, images):
-        """
-        For the array of images to show, images, go through either
-        creating a new dataitem or using an existing one if the image
-        is not null. The order of images is important, the channel is
-        used to find the appropriate dataitem.
-        """
+    def on_new_data_elements(self, data_elements):
         if not self.hardware_port:
-            images = []
+            data_elements = []
 
         # snapshots
         snapshot_count = len(self.__snapshots)
-        self.__snapshots.append(images)
+        self.__snapshots.append(data_elements)
         if len(self.__snapshots) != snapshot_count:
             self.notify_listeners("snapshot_count_changed", self.hardware_source, len(self.__snapshots))
 
         # update the data on the data items
-        self.update_images(images)
+        self.update_data_elements(data_elements)
 
         # notify listeners if we change current snapshot
         current_snapshot = len(self.__snapshots) - 1
@@ -381,14 +376,14 @@ class HardwareSourceDataBuffer(object):
             self.__current_snapshot = current_snapshot
             self.notify_listeners("current_snapshot_changed", self.hardware_source, self.__current_snapshot)
 
-    def update_images(self, images):
+    def update_data_elements(self, data_elements):
         # build useful data structures (channel -> data)
         channel_to_data_dict = {}
         channels = []
-        for channel, data in enumerate(images):
-            if data is not None:
+        for channel, data_element in enumerate(data_elements):
+            if data_element is not None:
                 channels.append(channel)
-                channel_to_data_dict[channel] = data
+                channel_to_data_dict[channel] = data_element["data"]
 
         # sync to data items
         new_channel_to_data_item_dict = self.document_controller.sync_channels_to_data_items(channels, self.data_group, str(self.hardware_source))
--- test/HardwareSource_test.py
+++ test/HardwareSource_test.py
@@ -3,62 +3,65 @@ import time
 
 import numpy as np
 
-from nion.swift import HardwareSource as HardwareSource
+from nion.swift import HardwareSource
 
 
-class SimpleHWSource(HardwareSource.HardwareSource):
-    def acquire(self):
-        SimpleHWSource.image += 1.0
+class SimpleHardwareSource(HardwareSource.HardwareSource):
+
+    def acquire_data_elements(self):
+        SimpleHardwareSource.image += 1.0
         time.sleep(0.2)
-        return [SimpleHWSource.image]
+        data_element = { "data": SimpleHardwareSource.image }
+        return [data_element]
 
     def __str__(self):
-        return "SimpleHWSource"
-SimpleHWSource.image = np.zeros(256)
+        return "SimpleHardwareSource"
+
+SimpleHardwareSource.image = np.zeros(256)
 
 
 class TestHardwareSourceClass(unittest.TestCase):
+
     def test_registration(self):
-        man = HardwareSource.HardwareSourceManager()
-        man._reset()
-        source = SimpleHWSource()
-        man.register_hardware_source(source)
-        self.assertEqual(len(man._all_hw_ports), 1)
-        p = man.create_port_for_name("TestCase", "SimpleHWSource")
+        hardware_source_manager = HardwareSource.HardwareSourceManager()
+        hardware_source_manager._reset()
+        source = SimpleHardwareSource()
+        hardware_source_manager.register_hardware_source(source)
+        self.assertEqual(len(hardware_source_manager._hardware_ports), 1)
+        p = hardware_source_manager.create_port_for_name("TestCase", "SimpleHardwareSource")
         self.assertIsNotNone(p)
         p.close()
-        man.unregister_hardware_source(source)
-        p = man.create_port_for_name("TestCase", "SimpleHWSource")
+        hardware_source_manager.unregister_hardware_source(source)
+        p = hardware_source_manager.create_port_for_name("TestCase", "SimpleHardwareSource")
         self.assertIsNone(p)
 
     def test_alias(self):
-        man = HardwareSource.HardwareSourceManager()
-        man._reset()
-        source = SimpleHWSource()
-        man.register_hardware_source(source)
-        man.make_hardware_source_alias("testalias", source)
-        man.make_hardware_source_alias("testalias2", source, "DiffProps", 0)
-        p = man.create_port_for_name("TestCase", "testalias")
+        hardware_source_manager = HardwareSource.HardwareSourceManager()
+        hardware_source_manager._reset()
+        source = SimpleHardwareSource()
+        hardware_source_manager.register_hardware_source(source)
+        hardware_source_manager.make_hardware_source_alias("testalias", source)
+        hardware_source_manager.make_hardware_source_alias("testalias2", source, "DiffProps", 0)
+        p = hardware_source_manager.create_port_for_name("TestCase", "testalias")
         self.assertIsNotNone(p)
         p.close()
-        p = man.create_port_for_name("TestCase", "testalias2")
+        p = hardware_source_manager.create_port_for_name("TestCase", "testalias2")
         self.assertIsNotNone(p)
         p.close()
 
     def test_events(self):
-        SimpleHWSource.image = np.zeros(256)
-        man = HardwareSource.HardwareSourceManager()
-        man._reset()
-        source = SimpleHWSource()
-        man.register_hardware_source(source)
-        self.assertEqual(len(man._all_hw_ports), 1)
-        p = man.create_port_for_name("TestCase", "SimpleHWSource")
-        def newims(images):
+        SimpleHardwareSource.image = np.zeros(256)
+        hardware_source_manager = HardwareSource.HardwareSourceManager()
+        hardware_source_manager._reset()
+        source = SimpleHardwareSource()
+        hardware_source_manager.register_hardware_source(source)
+        self.assertEqual(len(hardware_source_manager._hardware_ports), 1)
+        p = hardware_source_manager.create_port_for_name("TestCase", "SimpleHardwareSource")
+        def handle_new_data_elements(images):
             pass
-
-        p.on_new_images = newims
+        p.on_new_data_elements = handle_new_data_elements
         time.sleep(1) # wait for a second, we should have 4-6 images after this
-        tl_pixel = p.get_last_images()[0][0]
+        tl_pixel = p.get_last_data_elements()[0]["data"][0]
         #print "got %d images in 1s"%tl_pixel
         self.assertTrue(3.0 < tl_pixel < 7.0)
         p.close()
