50ea84d3d59f2233ca695ff9ad65330cada09a30
Chris Meyer <cmeyer1969@gmail.com>
2013-12-22 19:31:11 +0000

    Moved DisplayLimitsInspector into its own class.
    svn r1317

--- DataItemEditor.py
+++ DataItemEditor.py
@@ -1,14 +1,153 @@
 # standard libraries
 import gettext
+import logging
 
 # third party libraries
 # None
 
 # local libraries
 from nion.swift import DataItem
+from nion.swift import Decorators
+from nion.swift import Storage
 from nion.swift import UserInterfaceUtility
 
 _ = gettext.gettext
+
+
+
+class DisplayLimitsInspector(object):
+
+    """
+        Display limits binding is composed of a few fields:
+            display limits (read write): user specified display limits
+            data range (read only): the range of data, when converted to real numbers
+            display range (read only): display limits if present, otherwise data range
+    """
+
+    def __init__(self, ui, display_limits_binding):
+        self.ui = ui
+        self.display_limits_binding = display_limits_binding
+        self.display_limits_binding.add_listener(self)
+        self.__task_set = Decorators.TaskSet()
+        self.__block = False
+        # display limit editor
+        self.display_limits_section = self.ui.create_column_widget()
+        self.display_limits_section_title = self.ui.create_row_widget()
+        #self.display_limits_section_title.add(self.ui.create_label_widget(u"\u25B6", properties={"width": "20"}))
+        self.display_limits_section_title.add(self.ui.create_label_widget(_("Display Limits"), properties={"stylesheet": "font-weight: bold"}))
+        self.display_limits_section_title.add_stretch()
+        self.display_limits_section.add(self.display_limits_section_title)
+        self.display_limits_section_table = self.ui.create_row_widget()
+        self.display_limits_section_rows = self.ui.create_column_widget()
+        self.display_limits_section_table.add_spacing(20)
+        self.display_limits_section_table.add(self.display_limits_section_rows)
+        self.display_limits_range_row = self.ui.create_row_widget()
+        self.display_limits_range_low = self.ui.create_label_widget(properties={"width": 60})
+        self.display_limits_range_high = self.ui.create_label_widget(properties={"width": 60})
+        self.display_limits_range_row.add(self.ui.create_label_widget(_("Data Range:"), properties={"width": 120}))
+        self.display_limits_range_row.add(self.display_limits_range_low)
+        self.display_limits_range_row.add_spacing(8)
+        self.display_limits_range_row.add(self.display_limits_range_high)
+        self.display_limits_range_row.add_stretch()
+        self.display_limits_limit_row = self.ui.create_row_widget()
+        self.display_limits_limit_low = self.ui.create_line_edit_widget(properties={"width": 60})
+        self.display_limits_limit_high = self.ui.create_line_edit_widget(properties={"width": 60})
+        self.display_limits_limit_low.on_editing_finished = lambda text: self.display_limit_low_editing_finished(text)
+        self.display_limits_limit_high.on_editing_finished = lambda text: self.display_limit_high_editing_finished(text)
+        self.display_limits_limit_row.add(self.ui.create_label_widget(_("Display:"), properties={"width": 120}))
+        self.display_limits_limit_row.add(self.display_limits_limit_low)
+        self.display_limits_limit_row.add_spacing(8)
+        self.display_limits_limit_row.add(self.display_limits_limit_high)
+        self.display_limits_limit_row.add_stretch()
+        self.display_limits_section_rows.add_spacing(4)
+        self.display_limits_section_rows.add(self.display_limits_range_row)
+        self.display_limits_section_rows.add_spacing(4)
+        self.display_limits_section_rows.add(self.display_limits_limit_row)
+        self.display_limits_section.add(self.display_limits_section_table)
+        self.widget = self.display_limits_section
+
+    def close(self):
+        self.display_limits_binding.remove_listener(self)
+
+    def periodic(self):
+        self.__task_set.perform_tasks()
+
+    # this gets called from the display_limits_binding
+    def display_limits_changed(self):
+        if not self.__block:
+            self.__task_set.add_task("update", lambda: self.update())
+
+    def update(self):
+        # display limits
+        data_range = self.display_limits_binding.data_range
+        if data_range:
+            self.display_limits_range_low.text = "{0:.2f}".format(data_range[0])
+            self.display_limits_range_high.text = "{0:.2f}".format(data_range[1])
+        else:
+            self.display_limits_range_low.text = _("N/A")
+            self.display_limits_range_high.text = _("N/A")
+        display_range = self.display_limits_binding.display_range
+        if display_range:
+            self.display_limits_limit_low.text = "{0:.2f}".format(display_range[0])
+            self.display_limits_limit_high.text = "{0:.2f}".format(display_range[1])
+        else:
+            self.display_limits_limit_low.text = _("N/A")
+            self.display_limits_limit_high.text = _("N/A")
+        if self.display_limits_limit_low.focused:
+            self.display_limits_limit_low.select_all()
+        if self.display_limits_limit_high.focused:
+            self.display_limits_limit_high.select_all()
+
+    # handle display limit editing
+    def display_limit_low_editing_finished(self, text):
+        if self.display_limits_range_low.text != text:
+            display_limit_low = float(text)
+            block = self.__block
+            self.__block = True
+            self.display_limits_binding.display_limits = (display_limit_low, self.display_limits_binding.display_range[1])
+            self.__block = block
+            self.update()  # clean up displayed values
+
+    def display_limit_high_editing_finished(self, text):
+        if self.display_limits_range_high.text != text:
+            display_limit_high = float(text)
+            block = self.__block
+            self.__block = True
+            self.display_limits_binding.display_limits = (self.display_limits_binding.display_range[0], display_limit_high)
+            self.__block = block
+            self.update()  # clean up displayed values
+
+
+class DisplayLimitsBinding(Storage.Broadcaster):
+
+    def __init__(self, data_item):
+        super(DisplayLimitsBinding, self).__init__()
+        self.__data_item = data_item
+        # make sure we're listening to changes of the data item
+        self.__data_item.add_listener(self)
+
+    def close(self):
+        # unlisten to data item
+        self.__data_item.remove_listener(self)
+
+    # this will typically happen on a thread
+    def data_item_content_changed(self, data_item, changes):
+        if any (k in changes for k in (DataItem.DISPLAY, )):
+            self.notify_listeners("display_limits_changed")
+
+    def __get_data_range(self):
+        return self.__data_item.data_range
+    data_range = property(__get_data_range)
+
+    def __get_display_range(self):
+        return self.__data_item.display_range
+    display_range = property(__get_display_range)
+
+    def __get_display_limits(self):
+        return self.__data_item.display_limits
+    def __set_display_limits(self, display_limits):
+        self.__data_item.display_limits = display_limits
+    display_limits = property(__get_display_limits, __set_display_limits)
 
 
 class DataItemEditor(object):
@@ -99,43 +238,6 @@ class DataItemEditor(object):
         self.calibrations_section.add(self.calibrations_table)
         self.widget.add(self.calibrations_section)
 
-    def __init_display_limit_editor(self):
-        # display limit editor
-        self.display_limits_section = self.ui.create_column_widget()
-        self.display_limits_section_title = self.ui.create_row_widget()
-        #self.display_limits_section_title.add(self.ui.create_label_widget(u"\u25B6", properties={"width": "20"}))
-        self.display_limits_section_title.add(self.ui.create_label_widget(_("Display Limits"), properties={"stylesheet": "font-weight: bold"}))
-        self.display_limits_section_title.add_stretch()
-        self.display_limits_section.add(self.display_limits_section_title)
-        self.display_limits_section_table = self.ui.create_row_widget()
-        self.display_limits_section_rows = self.ui.create_column_widget()
-        self.display_limits_section_table.add_spacing(20)
-        self.display_limits_section_table.add(self.display_limits_section_rows)
-        self.display_limits_range_row = self.ui.create_row_widget()
-        self.display_limits_range_low = self.ui.create_label_widget(properties={"width": 60})
-        self.display_limits_range_high = self.ui.create_label_widget(properties={"width": 60})
-        self.display_limits_range_row.add(self.ui.create_label_widget(_("Data Range:"), properties={"width": 120}))
-        self.display_limits_range_row.add(self.display_limits_range_low)
-        self.display_limits_range_row.add_spacing(8)
-        self.display_limits_range_row.add(self.display_limits_range_high)
-        self.display_limits_range_row.add_stretch()
-        self.display_limits_limit_row = self.ui.create_row_widget()
-        self.display_limits_limit_low = self.ui.create_line_edit_widget(properties={"width": 60})
-        self.display_limits_limit_high = self.ui.create_line_edit_widget(properties={"width": 60})
-        self.display_limits_limit_low.on_editing_finished = lambda text: self.display_limit_low_editing_finished(text)
-        self.display_limits_limit_high.on_editing_finished = lambda text: self.display_limit_high_editing_finished(text)
-        self.display_limits_limit_row.add(self.ui.create_label_widget(_("Display:"), properties={"width": 120}))
-        self.display_limits_limit_row.add(self.display_limits_limit_low)
-        self.display_limits_limit_row.add_spacing(8)
-        self.display_limits_limit_row.add(self.display_limits_limit_high)
-        self.display_limits_limit_row.add_stretch()
-        self.display_limits_section_rows.add_spacing(4)
-        self.display_limits_section_rows.add(self.display_limits_range_row)
-        self.display_limits_section_rows.add_spacing(4)
-        self.display_limits_section_rows.add(self.display_limits_limit_row)
-        self.display_limits_section.add(self.display_limits_section_table)
-        self.widget.add(self.display_limits_section)
-
     def __init__(self, ui, data_item):
         self.ui = ui
         self.data_item = data_item
@@ -149,7 +251,10 @@ class DataItemEditor(object):
 
         self.__init_calibration_editor()
 
-        self.__init_display_limit_editor()
+        self.__display_limits_binding = DisplayLimitsBinding(self.data_item)
+        self.__display_limits_inspector = DisplayLimitsInspector(self.ui, self.__display_limits_binding)
+
+        self.widget.add(self.__display_limits_inspector.widget)
 
         # first update to get the values right
         self.__block = False
@@ -162,11 +267,16 @@ class DataItemEditor(object):
     def close(self):
         # unlisten to the data item
         self.data_item.remove_listener(self)
+        # close individual inspectors
+        self.__display_limits_inspector.close()
+        # close the bindings
+        self.__display_limits_binding.close()
 
     # update the values if needed
     def periodic(self):
         if self.needs_update:
             self.update()
+        self.__display_limits_inspector.periodic()
 
     # this will typically happen on a thread
     def data_item_content_changed(self, data_source, changes):
@@ -253,29 +363,12 @@ class DataItemEditor(object):
             calibration_row.children[2].text = "{0:.2f}".format(calibration.scale)
             calibration_row.children[3].text = calibration.units
 
-    def __update_display_limits_editor(self):
-        # display limits
-        data_range = self.data_item.data_range
-        if data_range:
-            self.display_limits_range_low.text = "{0:.2f}".format(data_range[0])
-            self.display_limits_range_high.text = "{0:.2f}".format(data_range[1])
-        else:
-            self.display_limits_range_low.text = _("N/A")
-            self.display_limits_range_high.text = _("N/A")
-        display_range = self.data_item.display_range
-        if display_range:
-            self.display_limits_limit_low.text = "{0:.2f}".format(display_range[0])
-            self.display_limits_limit_high.text = "{0:.2f}".format(display_range[1])
-        else:
-            self.display_limits_limit_low.text = _("N/A")
-            self.display_limits_limit_high.text = _("N/A")
-
     # update will NEVER be called on a thread
     def update(self):
         self.__update_info_editor()
         self.__update_param_editor()
         self.__update_calibrations_editor()
-        self.__update_display_limits_editor()
+        self.__display_limits_inspector.update()
         self.needs_update = False
 
     # handle param editing
@@ -316,25 +409,3 @@ class DataItemEditor(object):
         line_edit_widget = self.calibrations_column.children[calibration_index].children[3]
         if line_edit_widget.focused:
             line_edit_widget.select_all()
-
-    # handle display limit editing
-    def display_limit_low_editing_finished(self, text):
-        if self.display_limits_range_low.text != text:
-            display_limit_low = float(text)
-            block = self.__block
-            self.__block = True
-            self.data_item.display_limits = (display_limit_low, self.data_item.display_range[1])
-            self.__block = block
-            self.update()  # clean up displayed values
-            if self.display_limits_range_low.focused:
-                self.display_limits_range_low.select_all()
-    def display_limit_high_editing_finished(self, text):
-        if self.display_limits_range_high.text != text:
-            display_limit_high = float(text)
-            block = self.__block
-            self.__block = True
-            self.data_item.display_limits = (self.data_item.display_range[0], display_limit_high)
-            self.__block = block
-            self.update()  # clean up displayed values
-            if self.display_limits_limit_high.focused:
-                self.display_limits_range_high.select_all()
