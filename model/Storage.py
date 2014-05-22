44326e554605bf60769ab107f1aeeaa6f68199a7
Chris Meyer <cmeyer1969@gmail.com>
2014-05-22 14:56:53 +0000

    Remove redundant ref count handling in Storage base class.
    svn r1837

--- model/Storage.py
+++ model/Storage.py
@@ -225,51 +225,12 @@ class StorageBase(Observable.Observable, Observable.Broadcaster, Observable.Refe
         self.__weak_parents = []
         self.__transaction_count = 0
         self.__transaction_count_mutex = threading.RLock()
-        self.__ref_count = 0
-        self.__ref_count_mutex = threading.RLock()  # access to the image
         self.__uuid = uuid.uuid4()
 
     def __del__(self):
         # There should not be listeners or references at this point.
         assert len(self.__weak_parents) == 0, '{0} still has parents'.format(self.__class__.__name__)
         super(StorageBase, self).__del__()
-
-    # Give subclasses a chance to clean up. This gets called when reference
-    # count goes to 0, but before deletion.
-    def about_to_delete(self):
-        pass
-
-    def ref(self):
-        class RefContextManager(object):
-            def __init__(self, item):
-                self.__item = item
-            def __enter__(self):
-                self.__item.add_ref()
-                return self
-            def __exit__(self, type, value, traceback):
-                self.__item.remove_ref()
-        return RefContextManager(self)
-
-    # Anytime you store a reference to this item, call add_ref.
-    # This allows the class to disconnect from its own sources
-    # automatically when the reference count goes to zero.
-    def add_ref(self):
-        with self.__ref_count_mutex:
-            self.__ref_count += 1
-
-    # Anytime you give up a reference to this item, call remove_ref.
-    def remove_ref(self):
-        with self.__ref_count_mutex:
-            assert self.__ref_count > 0, 'DataItem has no references'
-            self.__ref_count -= 1
-            if self.__ref_count == 0:
-                self.about_to_delete()
-
-    # Return the reference count, which should represent the number
-    # of places that this DataItem is stored by a caller.
-    def __get_ref_count(self):
-        return self.__ref_count
-    ref_count = property(__get_ref_count)
 
     # uuid property. read only.
     def __get_uuid(self):
