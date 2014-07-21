004c70ca8cda15d7022710e5a6b3c60b3eace580
Chris Meyer <cmeyer1969@gmail.com>
2014-07-21 20:03:40 +0000

    Only read processed items from storage cache if unknown.
    svn r2118

--- model/DataItemProcessor.py
+++ model/DataItemProcessor.py
@@ -12,6 +12,8 @@ class DataItemProcessor(object):
         self.__in_progress = False
         self.__finish_event = threading.Event()
         self.__finish_event.set()
+        self.__cached_value = None
+        self.__cached_value_dirty = None
 
     def close(self):
         self.__finish_event.wait()
@@ -34,6 +36,7 @@ class DataItemProcessor(object):
 
     def set_cached_value_dirty(self):
         self.item.set_cached_value_dirty(self.__cache_property_name)
+        self.__cached_value_dirty = True
 
     def get_calculated_data(self, ui, data):
         """ Subclasses must implement. """
@@ -47,7 +50,7 @@ class DataItemProcessor(object):
         raise NotImplementedError()
 
     def get_data(self, ui, completion_fn=None):
-        if self.item.is_cached_value_dirty(self.__cache_property_name):
+        if (self.__cached_value_dirty is not None and self.__cached_value_dirty) or self.item.is_cached_value_dirty(self.__cache_property_name):
             item = self.item  # hold it
             data_item = self.get_data_item()
             if not data_item.closed:
@@ -65,11 +68,15 @@ class DataItemProcessor(object):
                                 self.__finish_event.set()
                                 raise
                             self.item.set_cached_value(self.__cache_property_name, calculated_data)
+                            self.__cached_value = calculated_data
+                            self.__cached_value_dirty = False
                         else:
                             calculated_data = None
                         if calculated_data is None:
                             calculated_data = self.get_default_data()
                             self.item.remove_cached_value(self.__cache_property_name)
+                            self.__cached_value = None
+                            self.__cached_value_dirty = None
                         if completion_fn:
                             completion_fn(calculated_data)
                         with self.__mutex:
@@ -80,7 +87,7 @@ class DataItemProcessor(object):
                             self.__in_progress = True
                             self.__finish_event.clear()
                             self.item.add_shared_task(self.__cache_property_name, None, lambda: load_data_on_thread(item))
-        calculated_data = self.item.get_cached_value(self.__cache_property_name)
+        calculated_data = self.__cached_value  # self.item.get_cached_value(self.__cache_property_name)
         if calculated_data is not None:
             return calculated_data
         return self.get_default_data()
