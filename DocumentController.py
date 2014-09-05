fc925a23750c746b14a3b233095084fb2a8e0ce3
Chris Meyer <cmeyer1969@gmail.com>
2014-09-05 15:32:39 +0000

    Make 'liveness' the primary sorting key for data items.
    svn r2325

--- DocumentController.py
+++ DocumentController.py
@@ -339,7 +339,7 @@ class DocumentController(Observable.Broadcaster):
 
         def sort_by_date_key(data_item):
             """ A sort key to for the datetime_original field of a data item. """
-            return Utility.get_datetime_from_datetime_item(data_item.datetime_original)
+            return data_item.is_live, Utility.get_datetime_from_datetime_item(data_item.datetime_original)
 
         with binding.changes():  # change filter and sort together
             if data_group is not None:
