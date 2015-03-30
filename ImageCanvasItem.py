7b92ed902325ed68634ecf8ad1ca61676ab752e4
Chris Meyer <cmeyer1969@gmail.com>
2015-03-30 05:58:47 +0000

    Only update info if changed (partial fix). Rename 'update_kayer' param.
    svn r3053

--- ImageCanvasItem.py
+++ ImageCanvasItem.py
@@ -119,8 +119,9 @@ class InfoOverlayCanvasItem(CanvasItem.AbstractCanvasItem):
 
     @image_canvas_size.setter
     def image_canvas_size(self, value):
-        self.__image_canvas_size = value
-        self.update()
+        if self.__image_canvas_size is None or value != self.__image_canvas_size:
+            self.__image_canvas_size = value
+            self.update()
 
     @property
     def image_canvas_origin(self):
@@ -128,8 +129,9 @@ class InfoOverlayCanvasItem(CanvasItem.AbstractCanvasItem):
 
     @image_canvas_origin.setter
     def image_canvas_origin(self, value):
-        self.__image_canvas_origin = value
-        self.update()
+        if self.__image_canvas_origin is None or value != self.__image_canvas_origin:
+            self.__image_canvas_origin = value
+            self.update()
 
     def set_data_and_calibration(self, data_and_calibration):
         self.__data_and_calibration = data_and_calibration
@@ -341,7 +343,7 @@ class ImageCanvasItem(CanvasItem.LayerCanvasItem):
             self.__update_image_canvas_size()
 
     # update the image canvas origin and size
-    def __scroll_area_canvas_item_layout_updated(self, scroll_area_canvas_size, trigger_layout):
+    def __scroll_area_canvas_item_layout_updated(self, scroll_area_canvas_size, trigger_update):
         if not self.__data_and_calibration:
             self.__last_image_norm_center = (0.5, 0.5)
             self.__last_image_zoom = 1.0
@@ -358,15 +360,15 @@ class ImageCanvasItem(CanvasItem.LayerCanvasItem):
             else:
                 image_canvas_size = (scroll_area_canvas_size[1] * dimensional_shape[0] / dimensional_shape[1], scroll_area_canvas_size[1])
             image_canvas_origin = (scroll_area_canvas_size[0] * 0.5 - image_canvas_size[0] * 0.5, scroll_area_canvas_size[1] * 0.5 - image_canvas_size[1] * 0.5)
-            self.__composite_canvas_item.update_layout(image_canvas_origin, image_canvas_size, trigger_layout)
+            self.__composite_canvas_item.update_layout(image_canvas_origin, image_canvas_size, trigger_update)
         elif self.__image_canvas_mode == "fit":
             image_canvas_size = scroll_area_canvas_size
             image_canvas_origin = (0, 0)
-            self.__composite_canvas_item.update_layout(image_canvas_origin, image_canvas_size, trigger_layout)
+            self.__composite_canvas_item.update_layout(image_canvas_origin, image_canvas_size, trigger_update)
         elif self.__image_canvas_mode == "1:1":
             image_canvas_size = dimensional_shape
             image_canvas_origin = (scroll_area_canvas_size[0] * 0.5 - image_canvas_size[0] * 0.5, scroll_area_canvas_size[1] * 0.5 - image_canvas_size[1] * 0.5)
-            self.__composite_canvas_item.update_layout(image_canvas_origin, image_canvas_size, trigger_layout)
+            self.__composite_canvas_item.update_layout(image_canvas_origin, image_canvas_size, trigger_update)
         else:
             c = self.__last_image_norm_center
             dimensional_shape = dimensional_shape
@@ -376,7 +378,7 @@ class ImageCanvasItem(CanvasItem.LayerCanvasItem):
             image_canvas_origin_y = (scroll_area_canvas_size[0] * 0.5) - c[0] * canvas_rect[1][0] - canvas_rect[0][0]
             image_canvas_origin_x = (scroll_area_canvas_size[1] * 0.5) - c[1] * canvas_rect[1][1] - canvas_rect[0][1]
             image_canvas_origin = (image_canvas_origin_y, image_canvas_origin_x)
-            self.__composite_canvas_item.update_layout(image_canvas_origin, image_canvas_size, trigger_layout)
+            self.__composite_canvas_item.update_layout(image_canvas_origin, image_canvas_size, trigger_update)
         # the image will be drawn centered within the canvas size
         if dimensional_shape[0] > 0.0 and dimensional_shape[1] > 0.0:
             #logging.debug("scroll_area_canvas_size %s", scroll_area_canvas_size)
