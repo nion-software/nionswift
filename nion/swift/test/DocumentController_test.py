3040df9cbaa561b0aaba23692a41b9eb53628ba1
Chris Meyer <cmeyer1969@gmail.com>
2017-02-17 17:15:51 -0800

    Minor garbage collection test improvement (be sure to release data panel).

--- nion/swift/test/DocumentController_test.py
+++ nion/swift/test/DocumentController_test.py
@@ -124,6 +124,8 @@ class TestDocumentControllerClass(unittest.TestCase):
         display_panel = DisplayPanel.DisplayPanel(document_controller, dict())
         display_panel.set_displayed_data_item(data_item)
         self.assertIsNotNone(weak_data_item())
+        display_panel.close()
+        display_panel.canvas_item.close()
         document_controller.close()
         document_controller = None
         data_item = None
