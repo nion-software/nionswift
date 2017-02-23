d54124e7892a617d36a7c2875fc55095825df7d5
Chris Meyer <cmeyer1969@gmail.com>
2017-02-23 10:04:01 -0800

    Safely close document controller, even when test fails. Prevents test hangs.

--- nion/swift/test/InfoPanel_test.py
+++ nion/swift/test/InfoPanel_test.py
@@ -1,4 +1,5 @@
 # standard libraries
+import contextlib
 import unittest
 
 # third party libraries
@@ -26,31 +27,31 @@ class TestInfoPanelClass(unittest.TestCase):
     def test_cursor_over_1d_data_displays_without_exception(self):
         document_model = DocumentModel.DocumentModel()
         document_controller = DocumentController.DocumentController(self.app.ui, document_model, workspace_id="library")
-        display_panel = document_controller.selected_display_panel
-        data_item = DataItem.DataItem(numpy.zeros((1000, )))
-        document_model.append_data_item(data_item)
-        display_panel.set_displayed_data_item(data_item)
-        header_height = Panel.HeaderCanvasItem.header_height
-        display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
-        display_panel.display_canvas_item.mouse_entered()
-        display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
-        display_panel.display_canvas_item.mouse_exited()
-        document_controller.close()
+        with contextlib.closing(document_controller):
+            display_panel = document_controller.selected_display_panel
+            data_item = DataItem.DataItem(numpy.zeros((1000, )))
+            document_model.append_data_item(data_item)
+            display_panel.set_displayed_data_item(data_item)
+            header_height = Panel.HeaderCanvasItem.header_height
+            display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
+            display_panel.display_canvas_item.mouse_entered()
+            display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
+            display_panel.display_canvas_item.mouse_exited()
 
     def test_cursor_over_1d_data_displays_without_exception_when_not_displaying_calibration(self):
         document_model = DocumentModel.DocumentModel()
         document_controller = DocumentController.DocumentController(self.app.ui, document_model, workspace_id="library")
-        display_panel = document_controller.selected_display_panel
-        data_item = DataItem.DataItem(numpy.zeros((1000, )))
-        document_model.append_data_item(data_item)
-        data_item.data_sources[0].displays[0].dimensional_calibration_style = "relative-top-left"
-        display_panel.set_displayed_data_item(data_item)
-        header_height = Panel.HeaderCanvasItem.header_height
-        display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
-        display_panel.display_canvas_item.mouse_entered()
-        display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
-        display_panel.display_canvas_item.mouse_exited()
-        document_controller.close()
+        with contextlib.closing(document_controller):
+            display_panel = document_controller.selected_display_panel
+            data_item = DataItem.DataItem(numpy.zeros((1000, )))
+            document_model.append_data_item(data_item)
+            data_item.data_sources[0].displays[0].dimensional_calibration_style = "relative-top-left"
+            display_panel.set_displayed_data_item(data_item)
+            header_height = Panel.HeaderCanvasItem.header_height
+            display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
+            display_panel.display_canvas_item.mouse_entered()
+            display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
+            display_panel.display_canvas_item.mouse_exited()
 
     def test_cursor_over_1d_multiple_data_displays_without_exception(self):
         data_and_metadata = DataAndMetadata.new_data_and_metadata(numpy.zeros((4, 1000), numpy.float64), data_descriptor=DataAndMetadata.DataDescriptor(False, 1, 1))
@@ -81,79 +82,79 @@ class TestInfoPanelClass(unittest.TestCase):
     def test_cursor_over_3d_data_displays_without_exception(self):
         document_model = DocumentModel.DocumentModel()
         document_controller = DocumentController.DocumentController(self.app.ui, document_model, workspace_id="library")
-        display_panel = document_controller.selected_display_panel
-        data_item = DataItem.DataItem(numpy.zeros((10, 10, 4)))
-        document_model.append_data_item(data_item)
-        display_panel.set_displayed_data_item(data_item)
-        header_height = Panel.HeaderCanvasItem.header_height
-        display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
-        display_panel.display_canvas_item.mouse_entered()
-        display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
-        display_panel.display_canvas_item.mouse_exited()
-        document_controller.close()
+        with contextlib.closing(document_controller):
+            display_panel = document_controller.selected_display_panel
+            data_item = DataItem.DataItem(numpy.zeros((10, 10, 4)))
+            document_model.append_data_item(data_item)
+            display_panel.set_displayed_data_item(data_item)
+            header_height = Panel.HeaderCanvasItem.header_height
+            display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
+            display_panel.display_canvas_item.mouse_entered()
+            display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
+            display_panel.display_canvas_item.mouse_exited()
 
     def test_cursor_over_3d_data_displays_correct_ordering_of_indices(self):
         document_model = DocumentModel.DocumentModel()
         document_controller = DocumentController.DocumentController(self.app.ui, document_model, workspace_id="library")
-        display_panel = document_controller.selected_display_panel
-        data_item = DataItem.DataItem(numpy.ones((100, 100, 20)))
-        data_item.maybe_data_source.displays[0].dimensional_calibration_style = "pixels-top-left"
-        document_model.append_data_item(data_item)
-        display_panel.set_displayed_data_item(data_item)
-        header_height = Panel.HeaderCanvasItem.header_height
-        info_panel = document_controller.find_dock_widget("info-panel").panel
-        display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
-        display_panel.display_canvas_item.mouse_entered()
-        display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
-        document_controller.periodic()
-        self.assertEqual(info_panel.label_row_1.text, "Position: 0.0, 50.0, 50.0")
-        self.assertEqual(info_panel.label_row_2.text, "Value: 1")
-        self.assertIsNone(info_panel.label_row_3.text, None)
-        display_panel.display_canvas_item.mouse_exited()
-        document_controller.close()
+        with contextlib.closing(document_controller):
+            display_panel = document_controller.selected_display_panel
+            data_item = DataItem.DataItem(numpy.ones((100, 100, 20)))
+            data_item.maybe_data_source.displays[0].dimensional_calibration_style = "pixels-top-left"
+            document_model.append_data_item(data_item)
+            display_panel.set_displayed_data_item(data_item)
+            header_height = Panel.HeaderCanvasItem.header_height
+            info_panel = document_controller.find_dock_widget("info-panel").panel
+            display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
+            display_panel.display_canvas_item.mouse_entered()
+            display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
+            document_controller.periodic()
+            self.assertEqual(info_panel.label_row_1.text, "Position: 0.0, 50.0, 50.0")
+            self.assertEqual(info_panel.label_row_2.text, "Value: 1")
+            self.assertIsNone(info_panel.label_row_3.text, None)
+            display_panel.display_canvas_item.mouse_exited()
 
     def test_cursor_over_2d_data_sequence_displays_correct_ordering_of_indices(self):
         document_model = DocumentModel.DocumentModel()
         document_controller = DocumentController.DocumentController(self.app.ui, document_model, workspace_id="library")
-        display_panel = document_controller.selected_display_panel
-        data_and_metadata = DataAndMetadata.new_data_and_metadata(numpy.ones((20, 100, 100), numpy.float64), data_descriptor=DataAndMetadata.DataDescriptor(True, 0, 2))
-        data_item = DataItem.new_data_item(data_and_metadata)
-        document_model.append_data_item(data_item)
-        display = DataItem.DisplaySpecifier.from_data_item(data_item).display
-        display.sequence_index = 4
-        display.dimensional_calibration_style = "pixels-top-left"
-        display_panel.set_displayed_data_item(data_item)
-        header_height = Panel.HeaderCanvasItem.header_height
-        info_panel = document_controller.find_dock_widget("info-panel").panel
-        display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
-        display_panel.display_canvas_item.mouse_entered()
-        display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
-        document_controller.periodic()
-        self.assertEqual(info_panel.label_row_1.text, "Position: 50.0, 50.0, 4.0")
-        self.assertEqual(info_panel.label_row_2.text, "Value: 1")
-        self.assertIsNone(info_panel.label_row_3.text, None)
-        display_panel.display_canvas_item.mouse_exited()
-        document_controller.close()
+        with contextlib.closing(document_controller):
+            display_panel = document_controller.selected_display_panel
+            data_and_metadata = DataAndMetadata.new_data_and_metadata(numpy.ones((20, 100, 100), numpy.float64), data_descriptor=DataAndMetadata.DataDescriptor(True, 0, 2))
+            data_item = DataItem.new_data_item(data_and_metadata)
+            document_model.append_data_item(data_item)
+            display = DataItem.DisplaySpecifier.from_data_item(data_item).display
+            display.sequence_index = 4
+            display.dimensional_calibration_style = "pixels-top-left"
+            display_panel.set_displayed_data_item(data_item)
+            header_height = Panel.HeaderCanvasItem.header_height
+            info_panel = document_controller.find_dock_widget("info-panel").panel
+            display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
+            display_panel.display_canvas_item.mouse_entered()
+            display_panel.display_canvas_item.mouse_position_changed(500, 500, Graphics.NullModifiers())
+            document_controller.periodic()
+            self.assertEqual(info_panel.label_row_1.text, "Position: 50.0, 50.0, 4.0")
+            self.assertEqual(info_panel.label_row_2.text, "Value: 1")
+            self.assertIsNone(info_panel.label_row_3.text, None)
+            display_panel.display_canvas_item.mouse_exited()
 
     def test_cursor_over_4d_data_displays_correctly(self):
         document_model = DocumentModel.DocumentModel()
         document_controller = DocumentController.DocumentController(self.app.ui, document_model, workspace_id="library")
-        display_panel = document_controller.selected_display_panel
-        data = (numpy.random.randn(100, 100, 20, 20) * 100).astype(numpy.int32)
-        data_item = DataItem.DataItem(data)
-        display = data_item.maybe_data_source.displays[0]
-        display.dimensional_calibration_style = "pixels-top-left"
-        display.collection_index = 20, 30
-        document_model.append_data_item(data_item)
-        display_panel.set_displayed_data_item(data_item)
-        header_height = Panel.HeaderCanvasItem.header_height
-        info_panel = document_controller.find_dock_widget("info-panel").panel
-        display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
-        display_panel.display_canvas_item.mouse_entered()
-        display_panel.display_canvas_item.mouse_position_changed(400, 600, Graphics.NullModifiers())
-        document_controller.periodic()
-        self.assertEqual(info_panel.label_row_1.text, "Position: 8.0, 12.0, 30.0, 20.0")
-        self.assertEqual(info_panel.label_row_2.text, "Value: {}".format(data[20, 30, 12, 8]))
-        self.assertIsNone(info_panel.label_row_3.text, None)
-        display_panel.display_canvas_item.mouse_exited()
-        document_controller.close()
+        with contextlib.closing(document_controller):
+            display_panel = document_controller.selected_display_panel
+            data = (numpy.random.randn(100, 100, 20, 20) * 100).astype(numpy.int32)
+            data_item = DataItem.DataItem(data)
+            display = data_item.maybe_data_source.displays[0]
+            display.dimensional_calibration_style = "pixels-top-left"
+            display.collection_index = 20, 30
+            document_model.append_data_item(data_item)
+            display_panel.set_displayed_data_item(data_item)
+            header_height = Panel.HeaderCanvasItem.header_height
+            info_panel = document_controller.find_dock_widget("info-panel").panel
+            display_panel.canvas_item.root_container.canvas_widget.on_size_changed(1000, 1000 + header_height)
+            display_panel.display_canvas_item.mouse_entered()
+            display_panel.display_canvas_item.mouse_position_changed(400, 600, Graphics.NullModifiers())
+            document_controller.periodic()
+            self.assertEqual(info_panel.label_row_1.text, "Position: 8.0, 12.0, 30.0, 20.0")
+            self.assertEqual(info_panel.label_row_2.text, "Value: {}".format(data[20, 30, 12, 8]))
+            self.assertIsNone(info_panel.label_row_3.text, None)
+            display_panel.display_canvas_item.mouse_exited()
