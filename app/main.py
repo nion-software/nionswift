eabb3d0edeef13452c92e8c8533835f9ee67d5df
Chris Meyer <cmeyer1969@gmail.com>
2017-04-17 12:07:34 -0700

    Add the app launcher file (was in nuthatch).

---
+++ app/main.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import warnings
+
+def main(args, bootstrap_args):
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "nionutils"))
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "nionui"))
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "niondata"))
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "nionswift"))
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), "typeshed"))
+    # these imports need to occur AFTER the args are parsed and the path
+    # is updated accordingly.
+    from nion.swift import Facade
+    from nion.swift import Application
+    from nion.ui import Application as ApplicationUI
+    warnings.simplefilter("always", RuntimeWarning)
+    Facade.initialize()
+    app = Application.Application(ApplicationUI.make_ui(bootstrap_args), resources_path=os.path.dirname(__file__))
+    app.initialize()
+    return app
