61fbee600ec0de12c9701b100e0b608f1ee2ffa1
Chris Meyer <cmeyer1969@gmail.com>
2017-07-13 11:19:31 -0700

    Versioning 0.10.7

--- nion/swift/Application.py
+++ nion/swift/Application.py
@@ -52,7 +52,7 @@ class Application:
         self.ui = ui
         self.ui.persistence_root = "3"  # sets of preferences
         self.__resources_path = resources_path
-        self.version_str = "0.10.6"
+        self.version_str = "0.10.7"
         self.workspace_dir = None
 
         self.__event_loop = None
