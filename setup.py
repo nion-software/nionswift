c3ec9869084f1bf644f5e569c851149bbc0bd312
Chris Meyer <cmeyer1969@gmail.com>
2017-09-12 10:42:21 -0700

    Declare namespace packages. WIP.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setuptools.setup(
     name="nionswift",
     version="0.0.1",
     packages=["nion.swift", "nion.swift.model", "nionui_app.nionswift", "nionswift_plugin", "nionlib", "nion.typeshed"],
+    namespace_packages=["nion", "nionswift_plugin"],
     install_requires=['scipy', 'numpy', 'h5py', 'pytz', 'tzlocal', 'nionutils', 'niondata', 'nionui'],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha"
