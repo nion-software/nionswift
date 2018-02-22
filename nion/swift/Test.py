3b4bc10b3596c68830269294cf12ad688c81a233
Chris Meyer <cmeyer1969@gmail.com>
2018-02-21 20:17:01 -0800

    Rework Test class to search for packages rather than directories.

--- nion/swift/Test.py
+++ nion/swift/Test.py
@@ -1,10 +1,8 @@
-# futures
-from __future__ import absolute_import
-
 # standard libraries
 import importlib
 import inspect
 import os
+import sys
 import unittest
 
 # third party libraries
@@ -13,47 +11,40 @@ import unittest
 # local libraries
 from nion.swift.model import PlugInManager
 
-"""
-Running tests without Qt:
-
-import Application
-import TestUI
-import Test
-Application.Application(TestUI.UserInterface())
-Test.run_all_tests()
-Test.run_test("TestApplicationClass")
-"""
-
-
 
 suites = []
 suite_dict = {}
 alltests = None
 
-def append_test_suites(suites):
-    suites.append(suites)
 
 # scan through directory and look for tests (files ending in test.py)
 # load the module and add the tests
-def load_tests(base_path):
+def load_tests(packages):
     global suites
     global suite_dict
     global alltests
 
-    for subdir in ["nionswift/nion/swift", "niondata/nion/data", "nionutils/nion/utils", "nionui/nion/ui"]:
-        localpath = os.path.join(base_path, subdir)
-        for file in os.listdir(os.path.join(localpath, "test")):
-            if file.endswith("_test.py"):
-                module_name = "nion.{0}.test.".format(os.path.basename(subdir)) + file.replace(".py", "")
-                module = importlib.import_module(module_name)
-                for maybe_a_class in inspect.getmembers(module):
-                    if inspect.isclass(maybe_a_class[1]) and maybe_a_class[0].startswith("Test"):
-                        test_name = maybe_a_class[0]
-                        # It is a class... add it to the test suite.
-                        cls = getattr(module, test_name)
-                        suite = unittest.TestLoader().loadTestsFromTestCase(cls)
-                        suites.append(suite)
-                        suite_dict[test_name] = suite
+    base_prefix = sys.base_prefix
+    directories = set()
+    for path in sys.path:
+        if not path.startswith(base_prefix):
+            directories.add(path)
+    for package in packages:
+        for directory in directories:
+            test_dir = os.path.join(directory, *package.split("."), "test")
+            if os.path.isdir(test_dir):
+                for file in os.listdir(test_dir):
+                    if file.endswith("_test.py"):
+                        module_name = f"{package}.test.{file.replace('.py', '')}"
+                        module = importlib.import_module(module_name)
+                        for maybe_a_class in inspect.getmembers(module):
+                            if inspect.isclass(maybe_a_class[1]) and maybe_a_class[0].startswith("Test"):
+                                test_name = maybe_a_class[0]
+                                # It is a class... add it to the test suite.
+                                cls = getattr(module, test_name)
+                                suite = unittest.TestLoader().loadTestsFromTestCase(cls)
+                                suites.append(suite)
+                                suite_dict[test_name] = suite
 
     suites.extend(PlugInManager.test_suites())
 
