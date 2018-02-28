053ef37a1a135a9335678192ab2306787cb0cc32
Chris Meyer <cmeyer1969@gmail.com>
2018-02-28 11:40:50 -0800

    Split recorder into controller and UI; add test. Fix transaction bug.

--- nion/swift/RecorderPanel.py
+++ nion/swift/RecorderPanel.py
@@ -20,11 +20,163 @@ from nion.swift.model import DataItem
 _ = gettext.gettext
 
 
+class Recorder:
+
+    def __init__(self, document_model, data_item):
+        self.__document_model = document_model
+        self.__data_item = data_item
+
+        self.on_live_state_changed = None
+        self.on_recording_state_changed = None
+        self.on_data_item_removed = None
+
+        self.__recording_state = "stopped"
+        self.__recording_start = 0.0
+        self.__recording_index = 0
+        self.__recording_data_item = None
+        self.__recording_transaction = None
+        self.__recording_error = False
+        self.__recording_interval = None
+        self.__recording_count = None
+
+        def data_item_content_changed():
+            is_live = data_item.is_live
+            if not is_live:
+                self.__stop_recording()
+            if callable(self.on_live_state_changed):
+                self.on_live_state_changed(is_live)
+
+        # if the item source or recorded data item is removed, stop recording
+        # called when an item is removed from the document
+        def item_removed(key, value, index):
+            if value == self.__recording_data_item:
+                self.__stop_recording()
+            if value == self.__data_item:
+                self.__stop_recording()
+                if callable(self.on_data_item_removed):
+                    self.on_data_item_removed()
+
+        def data_changed():
+            current_xdata = self.__data_item.xdata
+            if current_xdata and not current_xdata.is_sequence:
+                hardware_source_metadata = current_xdata.metadata.get("hardware_source", dict())
+                valid_rows = hardware_source_metadata.get("valid_rows", 0) if "valid_rows" in hardware_source_metadata else current_xdata.data_shape[0]
+                if len(current_xdata.data_shape) == 1 or valid_rows >= current_xdata.data_shape[0]:
+                    self.__last_complete_xdata = current_xdata
+            else:
+                self.__recording_error = True
+
+        self.__library_item_changed_event_listener = data_item.library_item_changed_event.listen(data_item_content_changed)
+        self.__item_removed_event_listener = self.__document_model.item_removed_event.listen(item_removed)
+        self.__data_item_data_changed_event_listener = self.__data_item.data_changed_event.listen(data_changed)
+
+        self.__last_complete_xdata = None
+
+        data_item_content_changed()
+        data_changed()
+
+    def close(self):
+        self.__library_item_changed_event_listener.close()
+        self.__library_item_changed_event_listener = None
+        self.__item_removed_event_listener.close()
+        self.__item_removed_event_listener = None
+        self.__data_item_data_changed_event_listener.close()
+        self.__data_item_data_changed_event_listener = None
+
+    @property
+    def recording_state(self) -> str:
+        return self.__recording_state
+
+    def continue_recording(self, current_time: float) -> None:
+        if self.__recording_state == "recording":
+            if (current_time - self.__recording_start) // self.__recording_interval > self.__recording_index - 1:
+                self.__recording_last = current_time
+                # first create an empty data item to hold the recorded data if it doesn't already exist
+                if not self.__recording_data_item:
+                    data_item = DataItem.DataItem(large_format=True)
+                    data_item.ensure_data_source()
+                    data_item.title = _("Recording of ") + self.__data_item.title
+                    self.__document_model.append_data_item(data_item)
+                    self.__recording_data_item = data_item
+                # next grab the current data and stop if it is a sequence (can't record sequences)
+                current_xdata = self.__last_complete_xdata
+                if self.__recording_error:
+                    self.__stop_recording()
+                    return
+                if not current_xdata:
+                    # no first image yet
+                    return
+                # now record the new data. it may or may not be a new frame at this point.
+                last_xdata = self.__recording_data_item.xdata
+                self.__recording_index += 1
+                if current_xdata and last_xdata and current_xdata.data_shape == self.__recording_data_item.data_shape[
+                                                                                1:]:
+                    # continue, append the new data to existing data and update existing data item
+                    intensity_calibration = last_xdata.intensity_calibration
+                    dimensional_calibrations = last_xdata.dimensional_calibrations
+                    data_descriptor = last_xdata.data_descriptor
+                    sequence_xdata = DataAndMetadata.new_data_and_metadata(
+                        numpy.vstack([last_xdata.data, [current_xdata.data]]),
+                        intensity_calibration=intensity_calibration, dimensional_calibrations=dimensional_calibrations,
+                        data_descriptor=data_descriptor)
+                    self.__recording_data_item.set_xdata(sequence_xdata)
+                elif current_xdata and not last_xdata:
+                    # first acquisition, create the sequence
+                    intensity_calibration = current_xdata.intensity_calibration
+                    dimensional_calibrations = [Calibration.Calibration(scale=self.__recording_interval,
+                                                                        units="s")] + list(
+                        current_xdata.dimensional_calibrations)
+                    data_descriptor = DataAndMetadata.DataDescriptor(True,
+                                                                     current_xdata.data_descriptor.collection_dimension_count,
+                                                                     current_xdata.data_descriptor.datum_dimension_count)
+                    sequence_xdata = DataAndMetadata.new_data_and_metadata(current_xdata.data[numpy.newaxis, ...],
+                                                                           intensity_calibration=intensity_calibration,
+                                                                           dimensional_calibrations=dimensional_calibrations,
+                                                                           data_descriptor=data_descriptor)
+                    self.__recording_data_item.set_xdata(sequence_xdata)
+                    self.__recording_transaction = self.__document_model.item_transaction(self.__recording_data_item)
+                else:
+                    # something is amiss. stop.
+                    self.__stop_recording()
+                    return
+            # finally -- check if we've reached the maximum count
+            if self.__recording_index >= self.__recording_count:
+                self.__stop_recording()
+
+    def start_recording(self, recording_start: float, recording_interval: float, recording_count: int) -> None:
+        self.__recording_state = "recording"
+        self.__recording_start = recording_start
+        self.__recording_index = 0
+        self.__recording_error = False
+        self.__recording_interval = recording_interval
+        self.__recording_count = recording_count
+        if callable(self.on_recording_state_changed):
+            self.on_recording_state_changed(self.__recording_state)
+
+    def stop_recording(self):
+        self.__stop_recording()
+
+    def __stop_recording(self):
+        if self.__recording_state == "recording":
+            self.__recording_state = "stopped"
+            self.__recording_start = 0.0
+            self.__recording_index = 0
+            self.__recording_error = False
+            if self.__recording_data_item and self.__recording_transaction:
+                self.__recording_transaction.close()
+                self.__recording_transaction = None
+                self.__recording_data_item = None
+            if callable(self.on_recording_state_changed):
+                self.on_recording_state_changed(self.__recording_state)
+
+
 class RecorderDialog(Dialog.ActionDialog):
 
     def __init__(self, document_controller, data_item):
         ui = document_controller.ui
         super().__init__(ui, _("Recorder"), app=document_controller.app, parent_window=document_controller, persistent_id="Recorder" + str(data_item.uuid))
+
+        self.__recorder = Recorder(document_controller.document_model, data_item)
 
         self.ui = ui
         self.document_controller = document_controller
@@ -90,21 +242,11 @@ class RecorderDialog(Dialog.ActionDialog):
         button_row.add(column1)
         button_row.add_spacing(8)
 
-        self.__recording_state = "stopped"
-        self.__recording_last = 0.0
-        self.__recording_index = 0
-        self.__recording_data_item = None
-        self.__recording_transaction = None
-        self.__recording_error = False
-
-        self.__recording_interval = self.__recording_interval_property.value / 1000
-        self.__recording_count = self.__recording_count_property.value
-
         def record_pressed():
-            if self.__recording_state == "recording":
-                self.__stop_recording()
+            if self.__recorder.recording_state == "recording":
+                self.__recorder.stop_recording()
             else:
-                self.__begin_recording()
+                self.__recorder.start_recording(time.time(), self.__recording_interval_property.value / 1000, self.__recording_count_property.value)
 
         self.__record_button.on_clicked = record_pressed
 
@@ -113,113 +255,30 @@ class RecorderDialog(Dialog.ActionDialog):
         column.add(button_row)
         column.add_spacing(6)
 
-        def data_item_content_changed():
-            is_live = data_item.is_live
+        def live_state_changed(is_live: bool) -> None:
             self.__record_button.enabled = is_live
-            if not is_live:
-                self.__stop_recording()
 
-        # if the item source or recorded data item is removed, stop recording
-        # called when an item is removed from the document
-        def item_removed(key, value, index):
-            if value == self.__recording_data_item:
-                self.__stop_recording()
-            if value == self.__data_item:
-                self.__stop_recording()
-                self.request_close()
-
-        def data_changed():
-            current_xdata = self.__data_item.xdata
-            if current_xdata and not current_xdata.is_sequence:
-                hardware_source_metadata = current_xdata.metadata.get("hardware_source", dict())
-                valid_rows = hardware_source_metadata.get("valid_rows", 0) if "valid_rows" in hardware_source_metadata else current_xdata.data_shape[0]
-                if len(current_xdata.data_shape) == 1 or valid_rows >= current_xdata.data_shape[0]:
-                    self.__last_complete_xdata = current_xdata
+        def recording_state_changed(recording_state: str) -> None:
+            if recording_state == "recording":
+                self.__record_button.text = _("Stop")
             else:
-                self.__recording_error = True
+                self.__record_button.text = _("Record")
 
-        self.__library_item_changed_event_listener = data_item.library_item_changed_event.listen(data_item_content_changed)
-        self.__item_removed_event_listener = self.document_controller.document_model.item_removed_event.listen(item_removed)
-        self.__data_item_data_changed_event_listener = self.__data_item.data_changed_event.listen(data_changed)
+        def data_item_removed() -> None:
+            self.request_close()
 
-        self.__last_complete_xdata = None
+        self.__recorder.on_live_state_changed = live_state_changed
+        self.__recorder.on_recording_state_changed = recording_state_changed
+        self.__recorder.on_data_item_removed = data_item_removed
 
-        data_item_content_changed()
-        data_changed()
+        live_state_changed(data_item.is_live)
+        recording_state_changed("stopped")
 
     def close(self):
-        self.__library_item_changed_event_listener.close()
-        self.__library_item_changed_event_listener = None
-        self.__item_removed_event_listener.close()
-        self.__item_removed_event_listener = None
-        self.__data_item_data_changed_event_listener.close()
-        self.__data_item_data_changed_event_listener = None
+        self.__recorder.close()
+        self.__recorder = None
         super().close()
 
     def periodic(self):
         super().periodic()
-        if self.__recording_state == "recording":
-            current_time = time.time()
-            if current_time - self.__recording_last > self.__recording_interval:
-                self.__recording_last = current_time
-                # first create an empty data item to hold the recorded data if it doesn't already exist
-                if not self.__recording_data_item:
-                    data_item = DataItem.DataItem(large_format=True)
-                    data_item.ensure_data_source()
-                    data_item.title = _("Recording of ") + self.__data_item.title
-                    self.document_controller.document_model.append_data_item(data_item)
-                    self.__recording_data_item = data_item
-                # next grab the current data and stop if it is a sequence (can't record sequences)
-                current_xdata = self.__last_complete_xdata
-                if self.__recording_error:
-                    self.__stop_recording()
-                    return
-                if not current_xdata:
-                    # no first image yet
-                    return
-                # now record the new data. it may or may not be a new frame at this point.
-                last_xdata = self.__recording_data_item.xdata
-                self.__recording_index += 1
-                if current_xdata and last_xdata and current_xdata.data_shape == self.__recording_data_item.data_shape[1:]:
-                    # continue, append the new data to existing data and update existing data item
-                    intensity_calibration = last_xdata.intensity_calibration
-                    dimensional_calibrations = last_xdata.dimensional_calibrations
-                    data_descriptor = last_xdata.data_descriptor
-                    sequence_xdata = DataAndMetadata.new_data_and_metadata(numpy.vstack([last_xdata.data, [current_xdata.data]]), intensity_calibration=intensity_calibration, dimensional_calibrations=dimensional_calibrations, data_descriptor=data_descriptor)
-                    self.__recording_data_item.set_xdata(sequence_xdata)
-                elif current_xdata and not last_xdata:
-                    # first acquisition, create the sequence
-                    intensity_calibration = current_xdata.intensity_calibration
-                    dimensional_calibrations = [Calibration.Calibration(scale=self.__recording_interval, units="s")] + list(current_xdata.dimensional_calibrations)
-                    data_descriptor = DataAndMetadata.DataDescriptor(True, current_xdata.data_descriptor.collection_dimension_count, current_xdata.data_descriptor.datum_dimension_count)
-                    sequence_xdata = DataAndMetadata.new_data_and_metadata(current_xdata.data[numpy.newaxis, ...], intensity_calibration=intensity_calibration, dimensional_calibrations=dimensional_calibrations, data_descriptor=data_descriptor)
-                    self.__recording_data_item.set_xdata(sequence_xdata)
-                    self.__recording_transaction = self.__recording_data_item._enter_transaction_state()
-                else:
-                    # something is amiss. stop.
-                    self.__stop_recording()
-                    return
-            # finally -- check if we've reached the maximum count
-            if self.__recording_index >= self.__recording_count:
-                self.__stop_recording()
-
-    def __begin_recording(self):
-        self.__recording_state = "recording"
-        self.__recording_last = 0.0
-        self.__recording_index = 0
-        self.__recording_error = False
-        self.__recording_interval = self.__recording_interval_property.value / 1000
-        self.__recording_count = self.__recording_count_property.value
-        self.__record_button.text = _("Stop")
-
-    def __stop_recording(self):
-        if self.__recording_state == "recording":
-            self.__recording_state = "stopped"
-            self.__recording_last = 0.0
-            self.__recording_index = 0
-            self.__recording_error = False
-            if self.__recording_data_item and self.__recording_transaction:
-                self.__recording_transaction.close()
-                self.__recording_transaction = None
-                self.__recording_data_item = None
-            self.__record_button.text = _("Record")
+        self.__recorder.continue_recording(time.time())
---
+++ nion/swift/test/RecorderPanel_test.py
@@ -0,0 +1,85 @@
+# standard libraries
+import contextlib
+import unittest
+
+# third party libraries
+import numpy
+
+# local libraries
+from nion.swift import Application
+from nion.swift import Facade
+from nion.swift import RecorderPanel
+from nion.swift.model import DataItem
+from nion.swift.model import DocumentModel
+from nion.ui import TestUI
+
+
+Facade.initialize()
+
+class TestRecorderPanelClass(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Application.Application(TestUI.UserInterface(), set_global=False)
+
+    def tearDown(self):
+        pass
+
+    def test_recorder_records_live_data(self):
+        document_model = DocumentModel.DocumentModel()
+        with contextlib.closing(document_model):
+            data_item = DataItem.DataItem(numpy.ones((8, 8)))
+            document_model.append_data_item(data_item)
+            recorder = RecorderPanel.Recorder(document_model, data_item)
+            with contextlib.closing(recorder):
+                with document_model.data_item_live(data_item):
+                    count = 4
+                    recorder.start_recording(10, 1, count)
+                    for i in range(count):
+                        recorder.continue_recording(10 + i + 0.25)
+                        recorder.continue_recording(10 + i + 0.75)
+                        data_item.set_data(data_item.data + 1)
+            self.assertEqual(2, len(document_model.data_items))
+            recorded_data_item = document_model.data_items[1]
+            self.assertTrue(recorded_data_item.xdata.is_sequence)
+            self.assertEqual((4, 8, 8), recorded_data_item.xdata.dimensional_shape)
+
+    def test_recorder_puts_recorded_data_item_under_transaction(self):
+        document_model = DocumentModel.DocumentModel()
+        with contextlib.closing(document_model):
+            data_item = DataItem.DataItem(numpy.ones((8, 8)))
+            document_model.append_data_item(data_item)
+            recorder = RecorderPanel.Recorder(document_model, data_item)
+            with contextlib.closing(recorder):
+                with document_model.data_item_live(data_item):
+                    count = 4
+                    recorder.start_recording(10, 1, count)
+                    for i in range(count):
+                        recorder.continue_recording(10 + i + 0.25)
+                        data_item.set_data(data_item.data + 1)
+                        if i != count - 1:  # last iteration it should fall out of transaction state since it is finished
+                            self.assertTrue(document_model.data_items[1].in_transaction_state)
+                        else:
+                            self.assertFalse(document_model.data_items[1].in_transaction_state)
+            self.assertFalse(document_model.data_items[1].in_transaction_state)
+
+    def test_recorder_state_is_reported_properly(self):
+        document_model = DocumentModel.DocumentModel()
+        with contextlib.closing(document_model):
+            data_item = DataItem.DataItem(numpy.ones((8, 8)))
+            document_model.append_data_item(data_item)
+            recorder_state_ref = ["unknown"]
+            def recorder_state_changed(recorder_state): recorder_state_ref[0] = recorder_state
+            recorder = RecorderPanel.Recorder(document_model, data_item)
+            recorder.on_recording_state_changed = recorder_state_changed
+            with contextlib.closing(recorder):
+                with document_model.data_item_live(data_item):
+                    count = 4
+                    recorder.start_recording(10, 1, count)
+                    for i in range(count):
+                        recorder.continue_recording(10 + i + 0.25)
+                        data_item.set_data(data_item.data + 1)
+                        if i != count - 1:  # last iteration it should fall out of transaction state since it is finished
+                            self.assertEqual(recorder_state_ref[0], "recording")
+                        else:
+                            self.assertEqual(recorder_state_ref[0], "stopped")
+                self.assertEqual(recorder_state_ref[0], "stopped")
