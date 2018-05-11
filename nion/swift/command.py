7cc2411d4b51f1ef7b33699f96878ff28fdfd569
Chris Meyer <cmeyer1969@gmail.com>
2018-05-11 09:04:16 -0700

    Use tool to launch command rather than duplicating code.

--- nion/swift/command.py
+++ nion/swift/command.py
@@ -62,26 +62,13 @@ def bootstrap_main(args):
 
 def main():
 
-    # first attempt to launch using nionswift-tool
+    # first, attempt to launch using nionswift-tool
     if pkg_resources.Environment()["nionswift-tool"]:
-        if sys.platform == "darwin":
-            app_path = pkg_resources.resource_filename("nion.nionswift_tool", "macosx/Nion Swift.app")
-            exe_path = os.path.join(app_path, "Contents", "MacOS", "Nion Swift")
-        elif sys.platform == "linux":
-            app_dir = pkg_resources.resource_filename("nion.nionswift_tool", "linux")
-            exe_path = os.path.join(app_dir, "NionSwiftLauncher")
-        elif sys.platform == "win32":
-            app_dir = pkg_resources.resource_filename("nion.nionswift_tool", "windows")
-            exe_path = os.path.join(app_dir, "NionSwiftLauncher.exe")
-        else:
-            exe_path = None
-        if exe_path:
-            python_prefix = os.sys.prefix
-            proc = subprocess.Popen([exe_path, python_prefix] + ["nionui_app.nionswift"] + sys.argv[1:], universal_newlines=True)
-            proc.communicate()
-            return
+        from nion.nionswift_tool import command
+        command.launch(sys.argv)
+        return
 
-    # next attempt to launch using pyqt
+    # next, attempt to launch using pyqt
     success = False
     try:
         from PyQt5 import QtCore
