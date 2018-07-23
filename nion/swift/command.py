288cde63eae4f47fcef643dd79c0499c9f8c8413
Chris Meyer <cmeyer1969@gmail.com>
2018-07-23 14:45:30 -0700

    Update launcher with support for 3.7 (minor changes).

--- nion/swift/command.py
+++ nion/swift/command.py
@@ -52,7 +52,7 @@ def bootstrap_main(args):
     Return the main application object.
     """
     version_info = sys.version_info
-    if version_info.major != 3 or version_info.minor != 6:
+    if version_info.major != 3 or version_info.minor < 6:
         return None, "python36"
     main_fn = load_module_as_package("nionui_app.nionswift")
     if main_fn:
