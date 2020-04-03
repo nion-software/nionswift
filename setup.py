7dde7eca09a5565e66b0f5405512bcbdf5d09155
Chris Meyer <cmeyer1969@gmail.com>
2020-04-02 20:52:54 -0700

    Switch to declarative setup.cfg + pyproject file.

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-import setuptools
-import os
-
-setuptools.setup(
-    name="nionswift",
-    version="0.14.8",
-    author="Nion Software",
-    author_email="swift@nion.com",
-    description="Nion Swift: Scientific Image Processing",
-    long_description=open("README.rst").read(),
-    url="https://github.com/nion-software/nionswift",
-    packages=["nion.swift", "nion.swift.model", "nion.swift.test", "nionui_app.nionswift", "nionswift_plugin.none", "nionlib", "nion.typeshed"],
-    package_data={"nion.swift": ["resources/*"], "nion.swift.model": ["resources/color_maps/*"]},
-    install_requires=['scipy', 'numpy', 'h5py', 'pytz', 'tzlocal', 'imageio', 'pillow', 'nionutils>=0.3.20,<0.4.0', 'niondata>=0.13.10', 'nionui>=0.3.28', 'nionswift-io'],
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha"
-    ],
-    test_suite="nion.swift.test",
-    entry_points={
-        'console_scripts': [
-            'nionswift=nion.swift.command:main',
-            ],
-        },
-)
+if __name__ == "__main__":
+    setup()
