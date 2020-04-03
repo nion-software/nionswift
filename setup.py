a82b50af30467ae6b89998580493186f2b444be4
Chris Meyer <cmeyer1969@gmail.com>
2020-04-02 18:00:34 -0700

    Update run/test requirements to nionui < 0.4.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     url="https://github.com/nion-software/nionswift",
     packages=["nion.swift", "nion.swift.model", "nion.swift.test", "nionui_app.nionswift", "nionswift_plugin.none", "nionlib", "nion.typeshed"],
     package_data={"nion.swift": ["resources/*"], "nion.swift.model": ["resources/color_maps/*"]},
-    install_requires=['scipy', 'numpy', 'h5py', 'pytz', 'tzlocal', 'imageio', 'pillow', 'nionutils>=0.3.17,<0.4.0', 'niondata>=0.13.6', 'nionui>=0.3.25', 'nionswift-io'],
+    install_requires=['scipy', 'numpy', 'h5py', 'pytz', 'tzlocal', 'imageio', 'pillow', 'nionutils>=0.3.17,<0.4.0', 'niondata>=0.13.6', 'nionui>=0.3.25,<0.4.0', 'nionswift-io'],
     classifiers=[
         "Development Status :: 2 - Pre-Alpha"
     ],
