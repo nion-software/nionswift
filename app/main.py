ff030d80385776f68cf6c21a7797d80962fd2c3b
Chris Meyer <cmeyer1969@gmail.com>
2021-10-10 19:17:50 -0700

    Enable strict typing in Application.

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def main(args, bootstrap_args):
     from nion.ui import Application as ApplicationUI
     warnings.simplefilter("always", RuntimeWarning)
     Facade.initialize()
-    app = Application.Application(ApplicationUI.make_ui(bootstrap_args), resources_path=os.path.dirname(__file__))
+    app = Application.Application(ApplicationUI.make_ui(bootstrap_args))
     app.initialize()
     Facade.start_server()
     return app
