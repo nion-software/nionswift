da9f5de421c2b5ef2d3d54317a53d70e2c15b374
Chris Meyer <cmeyer1969@gmail.com>
2021-11-17 01:18:22 +0000

    Add codespace configuration (useful for doc editing).

---
+++ .devcontainer/postStartCommand.sh
@@ -0,0 +1,6 @@
+echo "POSTSTARTCOMMAND"
+#python -m pip install --upgrade pip
+#python -m pip install sphinx sphinx-copybutton
+#cd ..
+#python -m pip install -r nionswift/test-requirements.txt
+#python -m pip install -e nionswift/nionswift
