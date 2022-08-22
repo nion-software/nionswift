be808e0c4d7d7c36218dc7da899e5f5e6dbb098d
gosselind1 <dgosselin.p@gmail.com>
2022-08-22 15:05:18 -0400

    Added a Test to Validate Overhauled Interval Zooming Code with Multiple Intervals
    Added an additional test to check that multiple intervals are zoomed in upon correctly.

--- nion/swift/test/DisplayPanel_test.py
+++ nion/swift/test/DisplayPanel_test.py
@@ -2722,6 +2722,50 @@ class TestDisplayPanelClass(unittest.TestCase):
             self.assertAlmostEqual(((3 * 2) + 4) * 1.2, axes.uncalibrated_data_max)
             self.assertAlmostEqual((-3 - 5) * 1.2, axes.uncalibrated_data_min)
 
+    def test_stacked_lineplot_interval_zoom_with_different_intensity_calibrations_with_multiple_intervals(self):
+        with TestContext.create_memory_context() as test_context:
+            document_controller = test_context.create_document_controller()
+            document_model = document_controller.document_model
+            data1 = numpy.array([x for x in range(20)]) * 1
+            data_item1 = DataItem.DataItem(data1)
+            data_item1.set_xdata(DataAndMetadata.new_data_and_metadata(data1, Calibration.Calibration(0, 1, "z")))
+            data2 = numpy.array([x for x in range(20)]) * 2
+            data_item2 = DataItem.DataItem(data2)
+            data_item2.set_xdata(DataAndMetadata.new_data_and_metadata(data2, Calibration.Calibration(0, 1, "z")))
+
+            document_model.append_data_item(data_item1)
+            document_model.append_data_item(data_item2)
+            display_panel = document_controller.selected_display_panel
+            display_item = document_model.get_display_item_for_data_item(data_item1)
+            display_item.display_type = "line_plot"
+            display_panel.set_display_panel_display_item(display_item)
+            display_panel.handle_drop_display_item("plus", document_model.get_display_item_for_data_item(data_item2))
+            display_panel.update()
+            canvas_shape = (480, 640)
+            document_controller.show_display_item(display_item)
+            display_panel.display_canvas_item.layout_immediate(canvas_shape)
+            display_panel.display_canvas_item.prepare_display()  # force layout
+            display_panel.display_canvas_item.refresh_layout_immediate()
+            line_plot_canvas_item = typing.cast(LinePlotCanvasItem.LinePlotCanvasItem,
+                                                display_panel.display_canvas_item)
+
+            interval_pairs = [(.25, .33), (.35, .45), (.55, .66)]
+            index = 0
+            for interval in interval_pairs:
+                index += 1
+                interval_graphic = Graphics.IntervalGraphic()
+                interval_graphic.start = interval[0]
+                interval_graphic.end = interval[1]
+                display_item.add_graphic(interval_graphic)
+                display_item.graphic_selection.add(display_item.graphics.index(interval_graphic))
+            line_plot_canvas_item.handle_auto_display()
+            display_panel.display_canvas_item.prepare_display()  # force layout
+            axes = line_plot_canvas_item._axes
+            self.assertAlmostEqual(axes.drawn_left_channel, 1)
+            self.assertAlmostEqual(axes.drawn_right_channel, 17)
+            self.assertAlmostEqual(13 * 2 * 1.2, axes.uncalibrated_data_max)
+            self.assertAlmostEqual(0, axes.uncalibrated_data_min)  # since no negative values, our min point is 0
+
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
