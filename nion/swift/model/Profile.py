52dbe76f0ce2a31b05bb41c6837704bf77d2ce97
Chris Meyer <34664+cmeyer@users.noreply.github.com>
2024-11-13 16:37:21 -0800

    Add a switch project action, useful for testing.

--- nion/swift/model/Profile.py
+++ nion/swift/model/Profile.py
@@ -628,6 +628,18 @@ class Profile(Persistence.PersistentObject):
                 return project_reference
         return None
 
+    def get_project_reference_by_project_uuid(self, project_uuid: uuid.UUID) -> typing.Optional[ProjectReference]:
+        for project_reference in self.project_references:
+            if project_reference.project_uuid == project_uuid:
+                return project_reference
+        return None
+
+    def get_project_reference_by_path(self, path: pathlib.Path) -> typing.Optional[ProjectReference]:
+        for project_reference in self.project_references:
+            if project_reference.path == path:
+                return project_reference
+        return None
+
     def append_project_reference(self, project_reference: ProjectReference) -> None:
         assert not self.get_item_by_uuid("project_references", project_reference.uuid)
         assert project_reference.project_uuid not in {project_reference.project_uuid for project_reference in self.project_references}
