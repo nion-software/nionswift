87b982458baf5d0921e98712df2951d864fddd6a
Chris Meyer <34664+cmeyer@users.noreply.github.com>
2025-09-18 13:38:03 -0700

    Fix syntax error in changes log. Add minimal syntax checking test.

--- nion/swift/test/Application_test.py
+++ nion/swift/test/Application_test.py
@@ -1,21 +1,30 @@
 # standard libraries
+import json
 import logging
+import pathlib
+import typing
 import unittest
 
-# third party libraries
-# None
-
 # local libraries
-# None
+from nion.swift.test import TestContext
 
 
 class TestApplicationClass(unittest.TestCase):
 
-    def setUp(self):
-        pass
+    def setUp(self) -> None:
+        TestContext.begin_leaks()
+        self._test_setup = TestContext.TestSetup()
 
-    def tearDown(self):
-        pass
+    def tearDown(self) -> None:
+        self._test_setup = typing.cast(typing.Any, None)
+        TestContext.end_leaks(self)
+
+    def test_changes_json_file(self) -> None:
+        with TestContext.MemoryProfileContext() as profile_context:
+            changes_json_path = pathlib.Path(__file__).parent.parent / "resources" / "changes.json"
+            with changes_json_path.open() as changes_json_fp:
+                changes_data: typing.Sequence[typing.Mapping[str, typing.Any]] = json.load(changes_json_fp)
+
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
