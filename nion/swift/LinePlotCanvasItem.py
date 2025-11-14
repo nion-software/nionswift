115edba89d53c8306ce7b03c77993f829060c606
lisham2000 <luke.isham@bruker.com>
2025-11-14 13:20:47 +0000

    Add Datastyle Protocol to LineGraphCanvasItem.py

--- nion/swift/LinePlotCanvasItem.py
+++ nion/swift/LinePlotCanvasItem.py
@@ -1019,16 +1019,15 @@ class LinePlotCanvasItem(DisplayCanvasItem.DisplayCanvasItem):
             self.__tracking_start_calibrated_data_per_pixel = (self.__tracking_start_calibrated_data_max - self.__tracking_start_calibrated_data_min) / plot_height
             plot_origin = self.__line_graph_vertical_axis_group_canvas_item.map_to_canvas_item(Geometry.IntPoint(), self)
             plot_rect = v_axis_canvas_bounds.translated(plot_origin)
-            if 0.0 >= self.__tracking_start_calibrated_data_min and 0.0 <= self.__tracking_start_calibrated_data_max and not axes.data_style == "log":
-                calibrated_unit_per_pixel = (self.__tracking_start_calibrated_data_max - self.__tracking_start_calibrated_data_min) / (plot_rect.height - 1) if plot_rect.height > 1 else 1.0
-                calibrated_unit_per_pixel = calibrated_unit_per_pixel if calibrated_unit_per_pixel else 1.0  # handle case where calibrated_unit_per_pixel is zero
-                origin_offset_pixels = (0.0 - self.__tracking_start_calibrated_data_min) / calibrated_unit_per_pixel
-                calibrated_origin = self.__tracking_start_calibrated_data_min + origin_offset_pixels * self.__tracking_start_calibrated_data_per_pixel
-                self.__tracking_start_origin_y = origin_offset_pixels
-                self.__tracking_start_calibrated_origin = calibrated_origin
-            else:
-                self.__tracking_start_origin_y = 0  # the distance the origin is up from the bottom
-                self.__tracking_start_calibrated_origin = self.__tracking_start_calibrated_data_min
+            origin_display = axes.data_style.display_origin(self.__tracking_start_calibrated_data_min,
+                                                            self.__tracking_start_calibrated_data_max)
+
+            calibrated_unit_per_pixel = ((self.__tracking_start_calibrated_data_max - self.__tracking_start_calibrated_data_min) /
+                                         (plot_rect.height - 1) if plot_rect.height > 1 else 1.0)
+            calibrated_unit_per_pixel = calibrated_unit_per_pixel if calibrated_unit_per_pixel else 1.0
+            origin_offset_pixels = (origin_display - self.__tracking_start_calibrated_data_min) / calibrated_unit_per_pixel
+            self.__tracking_start_origin_y = origin_offset_pixels
+            self.__tracking_start_calibrated_origin = origin_display
 
     def continue_tracking(self, pos: Geometry.IntPoint, modifiers: UserInterface.KeyboardModifiers) -> bool:
         delegate = self.delegate
