f1c20b44b82549c354d42881164a29dd8fe4747f
Chris Meyer <34664+cmeyer@users.noreply.github.com>
2026-02-05 07:53:05 -0800

    Change project references to use project_reference_id as key instead of project UUID. Tests.
    This ensures that the project is mainly specified by the project path
    rather than the project uuid, which may be duplicateed depending on
    how users have copied projects on the drive.

--- nion/swift/test/Project_test.py
+++ nion/swift/test/Project_test.py
@@ -35,7 +35,7 @@ class TestProjectClass(unittest.TestCase):
         self._test_setup = typing.cast(typing.Any, None)
         TestContext.end_leaks(self)
 
-    def test_projects_with_duplicate_uuid_are_not_loaded(self):
+    def test_projects_with_duplicate_uuid_are_allowed_on_different_paths(self):
         with create_memory_profile_context() as profile_context:
             document_model = profile_context.create_document_model(auto_close=False)
             profile = profile_context.profile
@@ -44,13 +44,13 @@ class TestProjectClass(unittest.TestCase):
                 document_model.append_data_item(data_item)
                 # add a project reference; it won't be loaded until we reload below
                 TestContext.add_project_memory(profile, document_model._project.uuid)
-                self.assertEqual(1, len(profile.projects))
-            # reload; will try to load two projects with same uuid
+                self.assertEqual(2, len(profile.projects))
+            # reload; there will be two project references, but only one project loaded initially
             document_model = profile_context.create_document_model(auto_close=False)
-            profile = typing.cast(Profile.Profile, getattr(document_model, "_profile_for_test"))
             with document_model.ref():
-                self.assertEqual(1, len(profile.projects))
-                self.assertEqual(1, len(profile.projects[0].data_items))
+                profile = typing.cast(Profile.Profile, getattr(document_model, "_profile_for_test"))
+                self.assertEqual(2, len(profile.project_references))
+                self.assertEqual(1, len(document_model.data_items))
 
     def test_project_reloads_with_same_uuid(self):
         with create_memory_profile_context() as profile_context:
@@ -72,10 +72,11 @@ class TestProjectClass(unittest.TestCase):
         with create_memory_profile_context() as profile_context:
             document_controller = profile_context.create_document_controller(auto_close=False)
             document_model = document_controller.document_model
-            TestContext.add_project_memory(profile_context.profile)
+            project_reference1 = TestContext.add_project_memory(profile_context.profile)
             with contextlib.closing(document_controller):
                 project_reference = TestContext.MemoryProjectReference()
-                project_reference.project_uuid = document_model._project.uuid
+                project_reference.project_uuid = project_reference1.project_uuid
+                project_reference._path = project_reference1._path
                 with self.assertRaises(Exception):
                     profile_context.profile.append_project_reference(project_reference)
                 project_reference.close()
